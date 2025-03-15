37c32410b8cb669bde5a743dce8f6c338d0afa83
Naman Dadhich <namandadhich15592@gmail.com>
2025-03-16 01:11:50 +0530

    my work

---
+++ src/pages/Career.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import JoinUs from '../components/JoinUs'
+
+const Career = () => {
+  return (
+    <div className='bg-[#121212] h-screen'>
+      <JoinUs />
+    </div>
+  )
+}
+
+export default Career
