c396d6d88f2a96b3c73ab63b68204ece63fe8428
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-03-18 23:50:47 +0530

    Animations

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { IoShareSocial } from "react-icons/io5";
+import { motion } from "framer-motion";
 
 const upcomingEvents = [
   {
@@ -7,7 +8,7 @@ const upcomingEvents = [
     date: "March 18, 2025",
     venue: "New Delhi",
     details:
-      "🚀 Code With DCG – Where Innovation Meets Impact! 🚀Where geeks build, innovators rise, and code shapes the future!Are you ready to push the boundaries of technology, fuel your brain with caffeine, and bring your wildest ideas to life? Code With DCG isn’t just a hackathon—it’s a 24-hour adrenaline rush of coding, creativity, and problem-solving, brought to you by Geek Room & DCG at Delhi Technological University.",
+      "🚀 Code With DCG – Where Innovation Meets Impact! 🚀Where geeks build, innovators rise, and code shapes the future!Are you ready to push the boundaries of technology.",
     social: "https://code-with-dcg.devfolio.co/",
     image: "/DGC.jpg",
   },
@@ -17,7 +18,7 @@ const upcomingEvents = [
     venue: "Jaipur",
     social: "https://codemanipal.devfolio.co/",
     details:
-      "🌟 Code-ए-Manipal: Where Innovation Meets Scarcity of Water 🌟Are you ready to unleash your inner tech wizard, survive on caffeine, and redefine what it means to innovate?Code-ए-Manipal isn’t just a hackathon – it’s a 36-hour rollercoaster of creativity, chaos, and cutting-edge ideas, hosted by LearnIT MUJ and the tech rebels at Geek Room.",
+      "🌟 Code-ए-Manipal: Where Innovation Meets Scarcity of Water 🌟Are you ready to unleash your inner tech wizard, survive on caffeine, and redefine what it means to innovate? ",
     image: "/ab2.jpg",
   },
   {
@@ -26,8 +27,7 @@ const upcomingEvents = [
     venue: "Greater Noida",
     social: "https://code-nakshatra.devfolio.co/",
     details:
-      "Where Innovators Collide and Code Takes Flight🌟Code Nakshatra: Where Ideas Shine and Dreams Soar🌟Get ready to unlock your coding superpowers, fuel up on ideas (and maybe some coffee), and experience a hackathon like never before. Code Nakshatra isn’t just another tech event – it’s a 24-hour adventure in innovation, collaboration, and fierce competition, hosted by Code Rangers and Geek Room TIIPS.",
-    link: "https://code-nakshatra.xyz/",
+      "Where Innovators Collide and Code Takes Flight🌟Code Nakshatra: Where Ideas Shine and Dreams Soar🌟Get ready to unlock your coding superpowers, fuel up on ideas (and maybe some coffee), and experience a hackathon like never before. Code Nakshatra isn’t just another tech event.",
     image: "/Nakshatra.jpg",
   },
 ];
@@ -38,7 +38,7 @@ const pastEvents = [
     date: "May 15-19, 2024",
     venue: "Noida, India",
     details:
-      "🌟 Introducing Code Cubicle: Unlocking Collaboration & Innovation, One Cubicle at a Time!🚀 Join us for an unparalleled tech adventure at Code Cubicle, the ultimate hackathon brought to you by Geek Room. Dive into the heart of innovation and creativity as we redefine the essence of technology-driven solutions.",
+      "🌟 Introducing Code Cubicle: Unlocking Collaboration & Innovation, One Cubicle at a Time!🚀 Join us for an unparalleled tech adventure at Code Cubicle, the ultimate hackathon brought to you by Geek Room..",
     social: "https://code-cubicle.devfolio.co/",
     image: "/CC3.0.jpg",
   },
@@ -48,7 +48,7 @@ const pastEvents = [
     venue: "New Delhi",
     social: "https://code-kshetra-2.devfolio.co/",
     details:
-      "🌟 Code Kshetra 2.0: Where Innovation Meets Madness🌟Are you ready to unleash your inner tech wizard, survive on caffeine, and redefine what it means to innovate?Code Kshetra isn’t just a hackathon – it’s a 36-hour rollercoaster of creativity, chaos, and cutting-edge ideas, hosted by JIMS Sector-5 Rohini and the tech rebels at Geek Room.",
+      "🌟 Code Kshetra 2.0: Where Innovation Meets Madness🌟Are you ready to unleash your inner tech wizard, survive on caffeine, and redefine what it means to innovate?Code Kshetra isn’t just a hackathon.",
     link: "https://codekshetra2.geekroom.in/",
     image: "/ab7.jpg",
   },
@@ -58,7 +58,7 @@ const pastEvents = [
     venue: "New Delhi",
     social: "https://code-kshetra.devfolio.co/",
     details:
-      "🚀 Code Kshetra: Where Geeks Battle with Code & Innovation📌 Welcome to Code Kshetra, a groundbreaking hackathon jointly organised by JIMS Sector-5 Rohini and Geek Room. Immerse yourself in the heart of tech innovation as we present a unique experience, to redefine the boundaries of creativity and technology.",
+      "🚀 Code Kshetra: Where Geeks Battle with Code & Innovation📌 Welcome to Code Kshetra, a groundbreaking hackathon jointly organised by JIMS Sector-5 Rohini and Geek Room.",
     image: "/CK1.0.jpg",
   },
   {
@@ -67,82 +67,112 @@ const pastEvents = [
     venue: "New Delhi",
     social: "https://techminds.devfolio.co/",
     details:
-      "Join TechMinds, an exhilarating hackathon co-organized by Geek Room and the Artificial Intelligence Experience Centre, IITM Janakpuri. This event invites tech enthusiasts and innovators to push the boundaries of creativity and technology. Scheduled for 15th and 16th March 2024, this 30-hour offline event in Delhi will feature live project presentations, idea pitching sessions, and judging by an esteemed panel",
+      "Join TechMinds, an exhilarating hackathon co-organized by Geek Room and the Artificial Intelligence Experience Centre, IITM Janakpuri. This event invites tech enthusiasts and innovators to push the boundaries of creativity and technology. ",
     image: "/TechMinds.jpg",
   },
 ];
 
-const ongoingEvents = [
-  {
-    name: "Pears Global Hackathon",
-    date: "Feb 28 - March 11 , 2025",
-    venue: "Global - Online",
-    social: "https://lu.ma/9mqll98a",
-    details:
-      "Welcome to Pears Hackathon 2025, hosted in collaboration with Geek Room and Pears Technology! This global online hackathon invites participants from all over the world to innovate, collaborate, and build cutting-edge projects using Pears Technology.Whether you're a student, professional, or coding enthusiast, this is your chance to showcase your creativity, solve real-world problems, and compete for exciting rewards. With expert mentorship, challenging problem statements, and a collaborative innovation space, we aim to provide an unforgettable hackathon experience.",
-    link: "https://pearshack.geekroom.in/",
-    image: "/pears.jpg",
-  },
-];
-
-//Main Body :
+//Main function
 export default function Events() {
   return (
-    <div className="h-full relative bg-[#121212]">
-      {/* Events Page Banner */}
-      <section className="max-h-screen md:mb-5 bg-[#121212] flex flex-col items-center justify-center text-[#F2F2F2] px-4">
+    <div className="h-full relative bg-[#121212] md:py-5">
+      {/* Events Page Banner with Animation */}
+      <motion.section
+        initial={{ scale: 0.7, opacity: 0 }}
+        whileInView={{ scale: 1, opacity: 1 }}
+        transition={{ duration: 1.8, ease: "easeInOut" }}
+        viewport={{ once: false, amount: 0.4 }}
+        className="max-h-screen md:mb-8 flex flex-col items-center justify-center text-[#F2F2F2] px-4"
+      >
         <img
           src="./logo/transparent_geek_room_logo.svg"
           alt="Logo"
-          className="w-20 h-20 md:w-42 md:h-42 md:mb-4 mt-[6rem] mb-[5.5rem] md:my-4"
+          className="w-20 h-20 md:w-42 md:h-42 md:mb-1 mt-[7rem] mb-[5.5rem] md:my-14"
         />
-        <h1 className="text-[2.3rem] md:text-[8rem] font-satoshi-bold text-center tracking-tight mb-3 md:mb-4">
+        <h1 className="text-[2.3rem] md:text-[8rem] font-satoshi-bold text-center tracking-tight mb-3 md:mb-4 md:mt-[1rem]">
           EVENTS
         </h1>
         <p className="text-lg md:text-xl font-satoshi text-center mb-[6rem] md:mb-4">
           &apos;&apos;A community dedicated to helping each other get better at
           coding together&apos;&apos;
         </p>
-      </section>
-
-      <section className="text-[#F2F2F2] flex flex-col items-center p-5 md:p-11 md:mt-[6rem] mt-[7rem]">
-        <h1 className="text-[2.3rem] text-[#F2F2F2] md:ml-5 md:text-[6rem] font-satoshi-bold mb-5 md:mb-10 tracking-tighter text-center">
-          ONGOING EVENTS 🔥
-        </h1>
-        <div className="grid grid-cols-1 md:grid-cols-1 gap-5 md:gap-8 w-full mt-2 md:w-[36%]">
-          {ongoingEvents.map((event, index) => (
-            <EventCard key={index} event={event} />
-          ))}
-        </div>
-      </section>
+      </motion.section>
 
       {/* Upcoming Events */}
-      <section className="text-[#F2F2F2] flex flex-col items-center p-5 md:p-10 md:mt-[6rem] mt-[7rem]">
-        <h1 className="text-[2.3rem] md:text-[6rem] font-satoshi-bold mb-5 md:mb-10 tracking-tighter text-center">
+      <motion.section
+        initial={{ x: -100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        whileInView={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        viewport={{ once: false, amount: 0.5 }}
+        transition={{ duration: 1.5, ease: "easeOut" }}
+        className="text-[#F2F2F2] flex flex-col items-center p-5 md:p-10 md:mt-[6rem] mt-[5rem]"
+      >
+        <motion.h1
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          whileInView={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          viewport={{ once: false, amount: 0.5 }}
+          transition={{ duration: 1.5, ease: "easeOut" }}
+          className="text-[2.3rem] md:text-[6rem] font-satoshi-bold mb-5 md:mb-10 tracking-tighter text-center"
+        >
           UPCOMING EVENTS 🚀
-        </h1>
+        </motion.h1>
 
-        {/* Grid for both Desktop & Mobile */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 w-full md:w-[80%]">
           {upcomingEvents.map((event, index) => (
-            <EventCard key={index} event={event} />
+            <motion.div
+              key={index}
+              initial={{ x: -100, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              exit={{ opacity: 0 }}
+              viewport={{ once: false, amount: 0.5 }}
+              transition={{ duration: 1.2, ease: "easeOut" }}
+            >
+              <EventCard event={event} />
+            </motion.div>
           ))}
         </div>
-      </section>
+      </motion.section>
 
       {/* Past Events */}
-      <section className="min-h-screen text-white flex flex-col items-center p-5 md:p-10 md:mt-[6rem] mt-[7rem]">
-        <h1 className="text-[2.3rem] md:text-[6rem] font-satoshi-bold mb-5 md:mb-8 tracking-tighter text-center">
+      <motion.section
+        initial={{ x: 100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        whileInView={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        viewport={{ once: false, amount: 0.5 }}
+        transition={{ duration: 1.5, ease: "easeOut" }}
+        className="min-h-screen text-white flex flex-col items-center p-5 md:p-10 md:mt-[6rem] mt-[7rem]"
+      >
+        <motion.h1
+          inital={{ x: 100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          whileInView={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          viewport={{ once: false, amount: 0.5 }}
+          transition={{ duration: 1.5, ease: "easeOut" }}
+          className="text-[2.3rem] md:text-[6rem] font-satoshi-bold mb-5 md:mb-8 tracking-tighter text-center"
+        >
           PAST EVENTS 🌟
-        </h1>
+        </motion.h1>
 
-        {/* Grid for both Desktop & Mobile */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 w-full md:w-[80%]">
           {pastEvents.map((event, index) => (
-            <EventCard key={index} event={event} />
+            <motion.div
+              key={index}
+              initial={{ x: 100, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              exit={{ opacity: 0 }}
+              viewport={{ once: false, amount: 0.5 }}
+              transition={{ duration: 1.2, ease: "easeOut" }}
+            >
+              <EventCard event={event} />
+            </motion.div>
           ))}
         </div>
-      </section>
+      </motion.section>
     </div>
   );
 }
@@ -150,8 +180,7 @@ export default function Events() {
 // Event Card Component
 function EventCard({ event }) {
   return (
-    <div className="bg-[#F2F2F2] p-6 rounded-lg flex flex-col gap-3 relative transition-transform hover:scale-105">
-      {/* Profile + Event Name */}
+    <div className="bg-[#242424] p-6 rounded-lg flex flex-col gap-3 relative">
       <div className="flex items-center gap-4">
         <div className="h-34 w-34 object-cover bg-gray-300 rounded-full">
           <img
@@ -160,7 +189,7 @@ function EventCard({ event }) {
           />
         </div>
         <div>
-          <h2 className="text-lg md:text-2xl md:py-3 font-semibold text-black">
+          <h2 className="text-lg md:text-2xl md:py-3 font-semibold text-white">
             {event.name}
           </h2>
           <p className="text-sm md:text-lg text-[#F15A22]">
@@ -168,14 +197,10 @@ function EventCard({ event }) {
           </p>
         </div>
       </div>
-
-      {/* Details */}
-      <p className="text-md pt-3 text-[#121212]">{event.details}</p>
-
-      {/* Icons + Button */}
+      <p className="text-md pt-3 text-white">{event.details}</p>
       <div className="flex justify-between items-center mt-4">
         <div className="flex gap-2">
-          <div className="text-[#121212] rounded-full">
+          <div className="text-white rounded-full">
             <a href={event.link}>
               <IoShareSocial />
             </a>
